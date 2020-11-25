d339798c33e8adacd1e922592f4fd38a51bf9df4
Alexander <gwartneydev@gmail.com>
2020-11-24 22:50:13 -0800

    example assignment finished

--- basics-assignment-4-problem/app.js
+++ basics-assignment-4-problem/app.js
@@ -1,16 +1,29 @@
 const app = Vue.createApp({
     data() {
         return{
-
+            value:'',
+            visable: true,
+            inputBackgroundColor: ''
         }
     },
 
     methods:{
-        
+       togelVis(){
+           this.visable = !this.visable;
+       }
     },
 
     computed:{
-
+        paraClasses(){
+            return{
+                user1:this.value === 'user1',
+                user2:this.value === 'user2',
+                visable: this.visable,
+                hidden: !this.visable
+            }
+        }
     }
 
 })
+
+app.mount("#assignment");
