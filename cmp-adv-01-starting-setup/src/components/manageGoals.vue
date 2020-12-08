bdd5a8263751eb015a9c35f4506200d2a7c27432
Alex <gwartneydev@gmail.com>
2020-12-07 18:20:49 -0800

    added in telport method
    added in teleport method for dialog component with slots

--- cmp-adv-01-starting-setup/src/components/manageGoals.vue
+++ cmp-adv-01-starting-setup/src/components/manageGoals.vue
@@ -4,10 +4,15 @@
         <h2>Manage Goals</h2>
         <input type="text" ref="goal">
         <button @click="setGoal">Set Goal</button>
-        <error-alert v-if="inputIsInvalid">
-            <h2>Input is invalid!</h2>
-            <p>Please enter at least a few charecters...</p>
-        </error-alert>
+
+        <teleport to="body">
+            <error-alert v-if="inputIsInvalid">
+                <h2>Input is invalid!</h2>
+                <p>Please enter at least a few charecters...</p>
+                <button @click="confirmError">Okay</button>
+            </error-alert>
+        </teleport>
+
     </div>
 </template>
 
@@ -18,18 +23,22 @@ export default {
     
   components: { errorAlert },
 
-    methods:{
-        data(){
+    data(){
             return{
                 inputIsInvalid:false
             };
         },
-        
+    methods:{
         setGoal(){
             const enteredValue = this.$refs.goal.value;
             if(enteredValue == ''){
                  this.inputIsInvalid = true;
+
             }
+        },
+
+        confirmError(){
+            this.inputIsInvalid = false;
         }
     }
 }
