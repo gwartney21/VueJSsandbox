9dc1b23c152e04d409a5bf23e4286c408196ac85
Alex <gwartneydev@gmail.com>
2020-12-23 23:25:01 -0800

    added in slots for base compoents

--- CoachAppDemo/coachproj/src/pages/coaches/CoachesList.vue
+++ CoachAppDemo/coachproj/src/pages/coaches/CoachesList.vue
@@ -1,21 +1,39 @@
 /* eslint-disable */
 <template>
     <section>
+        <base-card>
         Filter
         <div class="controls">
-            <button>refresh</button>
-            <router-link to="/register">Register as Coach</router-link>
+            <base-button mode="outline">refresh</base-button>
+            <base-button link to="/register">Register as Coach</base-button>
         </div>
         <ul v-if="hasCoaches">
-            <li v-for="coach in filteredCoaches" :key="coach.id">
-                {{coach.firstName}}
-            </li>
+           <coach-item v-for="coach in filteredCoaches"
+           :key="coach.id"
+           :id="coach.id"
+           :first-name="coach.firstName"
+           :last-name="coach.lastName"
+           :rate="coach.hourlyRate"
+           :areas="coach.areas"
+           ></coach-item>
         </ul>
+        <h3 v-else>No coaches found</h3>
+        </base-card>
      </section>
 </template>
 
 <script>
+
+ import coachItem from '../../components/Coaches/CoachItem.vue';
+import BaseButton from '../../components/ui/BaseButton.vue';
+  
 export default {
+
+    components:{
+        coachItem,
+        BaseButton
+    },
+
     computed:{
         filteredCoaches(){
             return this.$store.getters['coaches/coaches'];
@@ -26,3 +44,16 @@ export default {
     }
 }
 </script>
+
+<style scoped>
+ul {
+  list-style: none;
+  margin: 0;
+  padding: 0;
+}
+
+.controls {
+  display: flex;
+  justify-content: space-between;
+}
+</style>
